7153cdd6b4e58b962aca481c1d88435c12629b3e
Paulo Nascimento <ppnascimentoo@hotmail.com>
2024-08-26 22:01:33 -0300

    frist commit

--- lib/services/transfer_service.dart
+++ lib/services/transfer_service.dart
@@ -2,17 +2,17 @@ import 'package:dart_bank/models/account.dart';
 
 void transferService(Account origin, Account destination, double value) {
 
-  // Não permiti a transferencia de valores entre a mesma conta
+  // Não permite a transferencia de valores entre a mesma conta
   if (origin == destination) {
     throw Exception("Erro! a conta origem é igual a conta de destinação.");
   }
 
-  // Não permiti a transferencia de valores menor ou igual a zero
+  // Não permite a transferencia de valores menor ou igual a zero
   if (value <= 0) {
     throw Exception("O valor da transferência deve ser maior que zero.");
   }
 
-  // Não permiti a transferencia de valores maiores que o saldo da conta de origem
+  // Não permite a transferencia de valores maiores que o saldo da conta de origem
   if (origin.getBalance() < value) {
     throw Exception("Saldo insuficiente na conta de origem.");
   }
